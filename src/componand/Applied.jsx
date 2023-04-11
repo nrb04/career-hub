73cea61cb3a2de00a6418c9660093fde9729ba22
nrb04 <nredoyan@gmail.com>
2023-04-12 03:28:25 +0600

    skelatondone

--- src/componand/Applied.jsx
+++ src/componand/Applied.jsx
@@ -4,17 +4,26 @@ import { Link, useParams } from 'react-router-dom';
 const Applied = () => {
   const appliedJobs = JSON.parse(localStorage.getItem('appliedJobs')) || [];
   const [jobs, setJobs] = useState(null);
+  const [filterValue, setFilterValue] = useState('all');
 
   useEffect(() => {
     fetch('/data.json')
       .then((response) => response.json())
       .then((data) => {
-        const filteredJobs = data.filter(job => appliedJobs.some(appliedJob => appliedJob.id === job.id));
+        const filteredJobs = data.filter(job => {
+          if (filterValue === 'all') {
+            return appliedJobs.some(appliedJob => appliedJob.id === job.id);
+          } else {
+            return job.remote_or_onsite === filterValue && appliedJobs.some(appliedJob => appliedJob.id === job.id);
+          }
+        });
         setJobs(filteredJobs);
       });
-  }, [appliedJobs]);
+  }, [appliedJobs, filterValue]);
 
-
+  const handleFilterChange = (event) => {
+    setFilterValue(event.target.value);
+  }
 
   if (!jobs) {
     return <div>Loading...</div>;
@@ -23,13 +32,20 @@ const Applied = () => {
   return (
     <div>
       <h2>Applied Jobs</h2>
+      <div>
+        <label htmlFor="filter">Filter by Remote/Onsite:</label>
+        <select id="filter" value={filterValue} onChange={handleFilterChange}>
+          <option value="all">All</option>
+          <option value="Remote">Remote</option>
+          <option value="Onsite">Onsite</option>
+        </select>
+      </div>
       {jobs.length > 0 ? (
         <ul>
           {jobs.map((job, index) => (
-              <li key={index}>
-                  <h3>{job.job_title}</h3>
-                <p>{job.company_name}</p>
-             <Link to={{ pathname: `/jobs/${job.id}`}}><button className='mt-2 px-4 py-1 bg-batam rounded-md outline-none focus:ring-4 shadow-lg  '>View details</button></Link> 
+            <li key={index}>
+              <h3><Link to={{ pathname: `/jobs/${job.id}`}}>{job.job_title}</Link></h3>
+              <p>{job.company_name}</p>
             </li>
           ))}
         </ul>
